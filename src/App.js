0d99d6f7a8744d336cbaaeceacd23fc918165ece
lokesh <lokeshkumar380@gmail.com>
2020-02-02 20:48:06 +0530

    app.js commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import Headingbar from "./components/HeadingBar/Headingbar.js";
 import ImageText from "./components/ImageText/ImageText.js";
 import Catalouge from "./components/Catalouge/Catalouge.js";
 import "./App.css";
+import Dashboard from "./components/Dashboard/dashboard.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 class App extends React.Component {
   render() {
     return (
       <div class="maincontainer">
-        <div class="row" style={{ height: "68px" }}>
-          <Headingbar />
-        </div>
-        <div class="row">
-          <SliderComponent />
-        </div>
-        <div class="row">
-          <ImageText />
-        </div>
-        <div class="row">
-          <Catalouge />
-        </div>
+        <Router>
+          <Switch>
+            <Route path="/home" component={App}>
+              <div className="row" style={{ height: "68px" }}>
+                <Headingbar />
+              </div>
+              <div className="row">
+                <SliderComponent />
+              </div>
+              <div className="row">
+                <ImageText />
+              </div>
+              <div className="row">
+                <Catalouge />
+              </div>
+            </Route>
+            <Route path="/admin" component={Dashboard}>
+              <Dashboard />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
